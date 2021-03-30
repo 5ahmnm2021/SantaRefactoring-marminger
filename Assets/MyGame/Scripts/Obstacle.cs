803b6be743ea6613f5467f64cdfb142753bd0b6d
maximaarminger <maxima.arminger@htl-salzburg.ac.at>
2021-03-30 22:04:12 +0200

    refac: MenuController.cs, deleted empty lines

--- Assets/MyGame/Scripts/Obstacle.cs
+++ Assets/MyGame/Scripts/Obstacle.cs
@@ -2,33 +2,29 @@
 
 public class Obstacle : MonoBehaviour
 {
-Rigidbody2D rb;
-[SerializeField] private float MoveSpeed;
+    Rigidbody2D rb;
+    [SerializeField] private float MoveSpeed;
 
-private void Awake()
-{
-    rb = GetComponent<Rigidbody2D>();
-}
-
-void Update()
-{
-
-    if(transform.position.x < -15f)
+    private void Awake()
     {
-        Destroy(gameObject);
-}
-  
-    if (transform.position.x > 15f)
-    {
-        Destroy(gameObject);
+        rb = GetComponent<Rigidbody2D>();
     }
 
+    void Update()
+    {
+        if(transform.position.x < -15f)
+        {
+            Destroy(gameObject);
+    }
+  
+        if (transform.position.x > 15f)
+        {
+            Destroy(gameObject);
+        }
 }
 
 private void FixedUpdate()
-{
-
-    rb.velocity = Vector2.left * MoveSpeed;
-
-}
+    {
+        rb.velocity = Vector2.left * MoveSpeed;
+    }
 }
